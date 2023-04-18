2064bc9230e68dd51b93e0a59576e01d146a1ac2
Logan <logan.markewich@live.com>
2023-04-18 14:25:44 -0600

    update note-taking tool names to be consistent

--- llama_agi/agi/tools/NoteTakingTools.py
+++ llama_agi/agi/tools/NoteTakingTools.py
@@ -5,7 +5,7 @@ from agi.utils import initialize_search_index
 note_index = initialize_search_index([])
 
 
-@tool
+@tool("Record Note")
 def record_note(note: str) -> str:
     """Useful for when you need to record a note or reminder for yourself to reference in the future."""
     global note_index
@@ -13,7 +13,7 @@ def record_note(note: str) -> str:
     return "Note successfully recorded."
 
 
-@tool
+@tool("Search Notes")
 def search_notes(query_str: str) -> str:
     """Useful for searching through notes that you previously recorded."""
     global note_index
